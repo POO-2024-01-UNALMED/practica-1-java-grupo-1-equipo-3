f7880c66fc38d3de0e919c2c730d84f0503275b0
seestradav <seestradav@unal.edu.co>
2024-07-24 18:54:06 -0500

    commit 1

--- src/gestorAplicacion/empleados/Meta.java
+++ src/gestorAplicacion/empleados/Meta.java
@@ -1,5 +1,154 @@
-package gestorAplicacion.empleados;
+/**
+ * Este módulo pertenece al paquete 'gestorAplicacion.empleados' y contiene la clase 'Meta', que define las 
+ * características y comportamientos de las diferentes metas que se les pueden dar a los empleados, estas metas
+ * permiten verificar el cumplimiento de estas metas y dar bonificaciones por su cumplimiento.
+ * 
+ * AUTORES: Sebastian Estrada Villa, Valentina Luján Robledo,
+ * Luis David Ramirez Gonzales, Santiago Ochoa Quintero
+ */
 
-public class Meta {
-    
-}
+
+ package gestorAplicacion.empleados;
+
+ import java.io.Serializable;
+ 
+ 
+ /**
+  * La clase 'Meta' representa una meta de rendimiento para los empleados, incluyendo el nivel de la meta, 
+  * el valor mínimo a alcanzar y la comisión por cumplir la meta.
+  * 
+  * Funcionalidades en las que se usa: 
+  *      - Pago de nomina
+  */
+ 
+ public class Meta implements Serializable{
+     
+     //ATRIBUTOS--------------------------------------------------------------------------------------------------------------------------------------
+     
+     // De instancia
+     private static final long serialVersionUID = 1L; // Versión del serializado asociada a esta clase
+     private String nivel;
+     private double minimo;
+     private double comision;
+ 
+ 
+     // CONSTRUCTOR-------------------------------------------------------------------------------------------------------------------------------------
+ 
+      /**
+      * Constructor de la clase Meta.
+      * 
+      * @param nivel Nivel de la meta (Por ejemplo: "Facil", "Intermedio", "Dificil")
+      * 
+      * @param minimo Valor mínimo a alcanzar para cumplir la meta
+      * 
+      * @param comision Comisión por cumplir la meta
+      */
+ 
+     public Meta(String nivel, double minimo, double comision) {
+        
+         this.nivel = nivel;
+         this.minimo = minimo;
+         this.comision = comision;
+     }
+ 
+ 
+ 
+     //MÉTODOS----------------------------------------------------------------------------------------------------------
+ 
+ 
+     /**
+      * Determina si se cumplió la meta esperada. 
+      * 
+      * @param valorAlcanzado Valor alcanzado por el empleado
+      * 
+      * @return true si el valor alcanzado es mayor o igual al mínimo, false en caso contrario
+      * 
+      * Funcionalidades en las que se usa: Pago de nomina
+      */
+     
+     public boolean cumplioMeta(double valorAlcanzado) {
+         
+         return valorAlcanzado >= minimo;
+     }
+ 
+ 
+ 
+     /**
+      * Calcula el porcentaje de cumplimiento de la meta.
+      * 
+      * @param valorAlcanzado Valor alcanzado por el empleado
+      * 
+      * @return Una cadena de texto que indica el porcentaje de cumplimiento de la meta. 
+      *         Si no se alcanzó el 100%, también incluye el porcentaje faltante y la cantidad faltante del valor mínimo.
+      * 
+      * Funcionalidades en las que se usa: Pago de nomina
+      */
+ 
+     public String porcentajeCumplimiento(double valorAlcanzado) {
+         
+         double porcentajeCumplido = (valorAlcanzado / minimo) * 100;
+         String mensaje = "Porcentaje de cumplimiento: " + porcentajeCumplido + "%";
+         
+         if (porcentajeCumplido < 100){
+             mensaje += "\nPorcentaje faltante: " + (100 - porcentajeCumplido) + "%";
+             mensaje += "\nCantidad faltante del indice indicado: " + (minimo - valorAlcanzado); 
+         }
+         
+         return mensaje;
+     }
+ 
+ 
+ 
+     /**
+      * Devuelve una representación en cadena del objeto Meta.
+      * 
+      * @return Cadena de texto con información sobre la meta
+      */
+     
+     public String toString(){
+         
+         return "\n"
+         + "Minimo requerido:                 " + minimo + "\n"
+         + "Bonificación por cumplimiento:    " + comision + "\n";
+     }
+ 
+ 
+ 
+     // GETTERS Y SETTERS---------------------------------------------------------------------------------------------------------
+     
+ 
+     public String getNivel() {
+         
+         return this.nivel;
+     }
+ 
+ 
+     public void setNivel(String nivel) {
+         
+         this.nivel = nivel;
+     }
+     
+ 
+     public double getMinimo() {
+         
+         return minimo;
+     }
+ 
+ 
+     public void setMinimo(double minimo) {
+ 
+         this.minimo = minimo;
+     }
+ 
+ 
+     public double getComision() {
+         
+         return comision;
+     }
+ 
+ 
+     public void setComision(double comision) {
+         
+         this.comision = comision;
+     }
+ }
--- src/gestorAplicacion/empleados/Transportador.java
+++ src/gestorAplicacion/empleados/Transportador.java
@@ -1,5 +1,172 @@
-package gestorAplicacion.empleados;
+/**
+ * Este módulo pertenece al paquete 'gestorAplicacion.empleados' y contiene  la clase 'Transportador', que hereda 
+ * de la clase 'Persona' y define las características y comportamientos del transportador que es el trabajador 
+ * encargado de transportar productos en determinados transportes.
+ * 
+ * AUTORES: Sebastian Estrada Villa, Valentina Luján Robledo, 
+ * Luis David Ramirez Gonzales, Santiago Ochoa Quintero
+ */
 
-public class Transportador {
-    
-}
+
+ package gestorAplicacion.empleados;
+
+ import java.util.ArrayList;
+ import java.util.List;
+ 
+ import gestorAplicacion.empresa.Fabrica;
+ import gestorAplicacion.externo.CuentaBancaria;
+ import gestorAplicacion.externo.Persona;
+ import gestorAplicacion.externo.Transporte;
+ 
+ 
+ /**
+  * La clase 'Transportador' representa a un empleado transportador en la empresa, que tiene metas de rendimiento 
+  * y puede pertenecer a una fábrica y tener un transporte asignado.
+  * 
+  * Funcionalidades en las que se usa: 
+  * 		- Pago de nomina
+  */
+ 
+ public class Transportador extends Persona{
+     
+     // ATRIBUTOS---------------------------------------------------------------------------------------------------------------------------------
+     
+     // De clase
+     private static ArrayList<Meta> metas = new ArrayList<Meta>(List.of(
+         new Meta("Facil",4,10000),
+         new Meta("intermedio",7,15000),
+         new Meta("Dificil",10,20000))); //Crear una lista inmutable
+     private static ArrayList<Transportador> listaTransportadores = new ArrayList<Transportador>();
+     
+     // De instancia
+     private Transporte transporte;
+     private Fabrica fabrica;
+ 
+ 
+     // CONSTRUCTORES---------------------------------------------------------------------------------------------------------------------------------------
+ 
+      /**
+      * Constructor que recibe todos los parámetros.
+      * 
+      * @param nombre Nombre del transportador
+      * 
+      * @param edad Edad del transportador
+      * 
+      * @param identificacion Identificación del transportador
+      * 
+      * @param cuentaBancaria Cuenta bancaria del transportador
+      * 
+      * @param transporte Transporte asignado al transportador
+      */
+ 
+     public Transportador(String nombre, int edad, int identificacion, CuentaBancaria cuentaBancaria,Transporte transporte) {
+         
+         super(nombre, edad, identificacion, cuentaBancaria);
+         this.transporte = transporte;
+         listaTransportadores.add(this);
+     }
+ 
+ 
+     /**
+      * Constructor sin parámetros.
+      */
+ 
+     public Transportador() {}
+     
+ 
+ 
+     // MÉTODOS---------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+     /**
+      * Realiza el pago de salario a un objeto de tipo transportador.
+      * 
+      * Este método esta sobre escrito de la clase Persona.
+      * 
+      * Se disminuye el saldo de la cuenta bancaria de la fábrica en el monto especificado
+      * y se incrementa el saldo de la cuenta bancaria del transportador en el mismo monto.
+      * 
+      * @param pago Monto del salario a pagar
+      * 
+      * Funcionalidades en las que se usa: Pago de nomina
+      */
+ 
+     @Override
+     public void recibirPagos(double pago) {
+         
+         fabrica.getCuentaBancaria().disminuirSaldo(pago);
+         super.getCuentaBancaria().incrementarSaldo(pago);
+     }
+     
+ 
+ 
+     /**
+      * Devuelve una representación en cadena del objeto Transportador.
+      * 
+      * @return Una cadena de texto con información sobre el transportador, incluyendo su nombre, edad,
+      * 		   identificación y el tipo de transporte asignado.
+      */
+     
+     @Override
+     public String toString() {
+         
+         return "\n"
+         + "Nombre: 			" + getNombre() + "\n"
+         + "Edad: 			" + getEdad() + "\n"
+         + "Cedula:			" + getIdentificacion() + "\n"
+         + "Transporte:	    " + getTransporte().getTipo() + "\n";
+         
+     }
+     
+ 
+     
+     // GETTERS Y SETTERS-------------------------------------------------------------------------------------------------------------------------------
+     
+     
+     public static ArrayList<Meta> getMetas(){
+         
+         return metas;
+     }
+ 
+ 
+     public static void setMetas(ArrayList<Meta> metas){
+         
+         Transportador.metas = metas;
+     }
+ 
+ 
+     public static ArrayList<Transportador> getListaTransportadores() {
+         
+         return listaTransportadores;
+     }
+ 
+ 
+     public static void setListaTransportadores(ArrayList<Transportador> listaTransportadores) {
+         
+         Transportador.listaTransportadores = listaTransportadores;
+     }
+ 
+ 
+     public Transporte getTransporte() {
+         
+         return transporte;
+     }
+ 
+ 
+     public void setTransporte(Transporte transporte) {
+         
+         this.transporte = transporte;
+     }
+ 
+ 
+     public Fabrica getFabrica() {
+         
+         return fabrica;
+     }
+ 
+ 
+     public void setFabrica(Fabrica fabrica) {
+         
+         this.fabrica = fabrica;
+     }
+ }
