ff113ca273bff644c476e8b2d344fee25dcf5e59
seestradav <seestradav@unal.edu.co>
2024-07-29 21:41:34 -0500

    commit 2

--- src/gestorAplicacion/externo/Parejas.java
+++ src/gestorAplicacion/externo/Parejas.java
@@ -1,5 +1,91 @@
-package gestorAplicacion.externo;
+/**
+ * Este módulo pertenece al 'paquete gestorAplicacion.externo' y contiene una clase genérica para representar
+ * pares de valores, similar a un diccionario. Incluye la clase 'Parejas' que permite almacenar pares de valores
+ * <clave, valor> y proporciona métodos para gestionar estos pares.
+ * 
+ * AUTORES: Sebastian Estrada Villa, Valentina Luján Robledo, 
+ * Luis David Ramirez Gonzales, Santiago Ochoa Quintero
+ */
 
-public class Parejas {
-    
-}
+
+ package gestorAplicacion.externo;
+
+ import java.io.Serializable;
+ 
+ 
+ /**
+  * La clase 'Parejas' representa un par de valores <clave, valor>.
+  * 
+  * Permite almacenar y gestionar pares de valores de cualquier tipo, similar a un diccionario.
+  * 
+  * Proporciona métodos para obtener y establecer la clave y el valor.
+  * 
+  * @param <Clave> Tipo de la clave
+  * 
+  * @param <Valor> Tipo del valor
+  */ 
+ 
+ public class Parejas<Clave, Valor> implements Serializable {
+     
+     // ATRIBUTOS ----------------------------------------------------------------------------------------------------------------------------------------------------
+     
+     // De clase 
+     private static final long serialVersionUID = 1L; // Versión del serializado asociada a esta clase
+     
+     // De instancia
+     // Representan las claves y valores de un diccionario
+     private Clave key;
+     private Valor value;
+     
+ 
+     // CONSTRUCTORES -----------------------------------------------------------------------------------------------------------------------
+     
+     /**
+      * Constructor que recibe los parámetros.
+      * 
+      * @param key Clave del par
+      * 
+      * @param value Valor del par
+      */
+ 
+     public Parejas(Clave key, Valor value) {
+         
+         this.key = key;
+         this.value = value;
+     }
+ 
+ 
+     /**
+      * Constructor vacío.
+      */
+ 
+     public Parejas(){}
+ 
+ 
+ 
+     // GETTERS Y SETTERS --------------------------------------------------------------------------------------------------------------------
+     
+     
+     public Clave getKey() {
+         
+         return key;
+     }
+ 
+     
+     public void setKey(Clave key) {
+         
+         this.key = key;
+     }
+ 
+     
+     public Valor getValue() {
+         
+         return value;
+     }
+ 
+     
+     public void setValue(Valor value) {
+         
+         this.value = value;
+     }
+ }
--- src/gestorAplicacion/externo/TipoTransporte.java
+++ src/gestorAplicacion/externo/TipoTransporte.java
@@ -1,5 +1,202 @@
-package gestorAplicacion.externo;
+/**
+ * Este módulo pertenece al paquete 'gestorAplicacion.externo' y contiene una enumeración para representar
+ * los diferentes tipos de transporte y sus atributos. Incluye la clase 'TipoTransporte' que define los diferentes
+ * tipos de transporte, sus capacidades y costos.
+ * 
+ * AUTORES: Sebastian Estrada Villa, Valentina Luján Robledo, 
+ * Luis David Ramirez Gonzales, Santiago Ochoa Quintero
+ */
 
-public enum TipoTransporte {
-    
-}
+
+ package gestorAplicacion.externo;
+
+ import java.util.ArrayList;
+ import java.util.Random;
+ 
+ import gestorAplicacion.empleados.Transportador;
+ 
+ 
+ /**
+  * La enumeración 'TipoTransporte' representa los diferentes tipos de transporte disponibles.
+  * 
+  * Contiene información sobre el costo de envío, la capacidad de carga y el nombre del transporte.
+  * 
+  * Proporciona métodos para gestionar estos atributos y seleccionar el transporte adecuado según la carga.
+  * 
+  * Funcionalidades en las que se usa:
+  *      - Envio pedidos
+  */
+ 
+ public enum TipoTransporte {
+     
+     //CONSTANTES -------------------------------------------------------------------------------------------------------------------
+     CAMINANDO(1000, 5,"Caminando"),
+     BICICLETA(2000,8,"Bicicleta"),
+     MOTO(5000, 10, "Moto"),
+     CARRO(10000,30,"Carro"),
+     CAMION(50000, 100, "Camion"),
+     AVION(500000, 1000,"Avion");
+     
+ 
+     // ATRIBUTOS -----------------------------------------------------------------------------------------------------------------------------
+     
+     // De instancia
+     double precioEnvio_COP; //pesos colombianos
+     double capacidad_KG; //kilogramos
+     String nombre;
+ 
+ 
+     // CONSTRUCTOR -----------------------------------------------------------------------------------------------------------------------------
+     
+     /**
+      * Constructor que recibe los parámetros:
+      * 
+      * @param precioEnvio Costo de envío en pesos colombianos
+      * 
+      * @param capacidad Capacidad de carga en kilogramos
+      * 
+      * @param nombre Nombre del tipo de transporte
+      */
+ 
+     private TipoTransporte(int precioEnvio, double capacidad, String nombre) {
+         
+         // Constructor privado para que solo se tengan las constantes definidas anteriormente    
+         this.precioEnvio_COP = precioEnvio;
+         this.capacidad_KG = capacidad;
+         this.nombre = nombre;
+     }
+ 
+ 
+ 
+     // MÉTODOS --------------------------------------------------------------------------------------------------------------------------------------
+     
+ 
+     /**
+      * Verifica qué tipos de transporte son aptos según el peso de los productos a enviar.
+      * 
+      * @param PesoTotalProductos Peso total de los productos a enviar
+      * 
+      * @return Una lista de tipos de transporte que pueden manejar el peso total de los productos
+      * 
+      * Funcionalidades en las que se usa: Envio pedidos
+      */
+ 
+     public static ArrayList<TipoTransporte> transporteSegunCarga(int PesoTotalProductos){
+         
+         ArrayList<TipoTransporte> transportesPosibles = new ArrayList<TipoTransporte>();
+         
+         for(TipoTransporte tipoTransporte : TipoTransporte.values()) {
+             
+             if(tipoTransporte.getCapacidad_KG() >= PesoTotalProductos){
+                 transportesPosibles.add(tipoTransporte); 
+             }
+     }
+ 
+     return transportesPosibles;
+     }
+ 
+ 
+     
+     /**
+      * Muestra los tipos de transporte disponibles según la carga.
+      * 
+      * @param transportesPosibles Lista de tipos de transporte posibles
+      * 
+      * @return Una cadena de texto con información sobre los tipos de transporte disponibles
+      * 
+      * Funcionalidades en las que se usa: Envio pedidos
+      */
+ 
+     public static String mostrarTransporteSegunCarga(ArrayList<TipoTransporte> transportesPosibles){
+         
+         String mensajeTransportesPosibles = "";
+         int numero = 1;
+         
+         for(TipoTransporte tipoTransportes : transportesPosibles) {
+             mensajeTransportesPosibles += numero + ". " + tipoTransportes.getNombre() + " $" + tipoTransportes.getPrecioEnvio_COP() + "\n";
+             numero++;
+         }
+         
+         return  mensajeTransportesPosibles;
+     }
+ 
+ 
+ 
+     /**
+      * Selecciona el transporte y le asigna un transportador aleatorio.
+      * 
+      * @param ListaFiltrada Lista de tipos de transporte filtrada según la carga
+      * 
+      * @param opcion Opción seleccionada por el usuario (1-based)
+      * 
+      * @return Un objeto Transporte con las características seleccionadas
+      * 
+      * Funcionalidades en las que se usa: Envio pedidos
+      */
+     public static Transporte seleccionarTransporte(ArrayList<TipoTransporte> ListaFiltrada,int opcion){
+ 
+         Random random = new Random();
+         int indice = random.nextInt(Transportador.getListaTransportadores().size()); //se genera un nuero aleatorio con un maximo en el tamaño de la lista de transportadores 
+         Transportador transportador = Transportador.getListaTransportadores().get(indice);
+         TipoTransporte tipo = ListaFiltrada.get(opcion-1);
+         Double capacidad= ListaFiltrada.get(opcion-1).getCapacidad_KG();
+         Double precioEnvio = ListaFiltrada.get(opcion-1).getPrecioEnvio_COP();
+         Transporte transporte = new Transporte(tipo,capacidad,precioEnvio,transportador); //Se crea un objeto de tipo Transporte con las caracteristicas requeridas
+         transportador.setTransporte(transporte);
+ 
+         return transporte;
+     }
+ 
+ 
+ 
+     /**
+      * Genera una representación en cadena del tipo de transporte.
+      * 
+      * @return Una cadena de texto con el nombre del tipo de transporte
+      */
+ 
+     @Override
+     public String toString() {
+         return this.getNombre();
+     }
+ 
+ 
+ 
+     //GETTERS Y SETTERS ------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+     public double getPrecioEnvio_COP() {
+         
+         return precioEnvio_COP;
+     }
+ 
+ 
+     public void setPrecioEnvio_COP(double precioEnvio) {
+         
+         this.precioEnvio_COP = precioEnvio;
+     }
+ 
+ 
+     public double getCapacidad_KG() {
+         
+         return capacidad_KG;
+     }
+ 
+ 
+     public void setCapacidad_KG(double capacidad) {
+         
+         this.capacidad_KG = capacidad;
+     }
+ 
+ 
+     public String getNombre() {
+         
+         return nombre;
+     }
+ 
+ 
+     public void setNombre(String nombre) {
+         
+         this.nombre = nombre;
+     }
+ }
