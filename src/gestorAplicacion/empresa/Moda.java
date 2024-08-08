fe3635b89a629483cd8d6adb5a5b396808570828
SantiagoOchoaQuintero <159163180+SantiagoOchoaQuintero@users.noreply.github.com>
2024-08-07 20:46:23 -0500

    Update Moda.java
    Creación clase moda

--- src/gestorAplicacion/empresa/Moda.java
+++ src/gestorAplicacion/empresa/Moda.java
@@ -1,5 +1,34 @@
+/**
+ * Este módulo pertenece al paquete 'gestorAplicacion.empresa' y contiene La interfaz 'Moda' que 
+ * representa un tipo de dato que define un nombre. Es utilizada para obtener el nombre de los 
+ * elementos que se desean analizar y encontrar la moda entre un conjunto de datos.
+ * 
+ * AUTORES: Sebastian Estrada Villa, Valentina Luján Robledo, 
+ * Luis David Ramirez Gonzales, Santiago Ochoa Quintero
+ */
+
+
 package gestorAplicacion.empresa;
 
-public interface Moda {
-    
+
+ /**
+  * 'Moda' representa una interfaz que permite seleccionar objetos para poder encontrar la moda en 
+  * la funcionalidad de evaluación operación. Esta interfaz tiene un único método llamado 'getNombre'.
+  */
+ 
+public abstract interface Moda {
+ 
+     // MÉTODOS ------------------------------------------------------------------------------------------------------------------------------------------
+     
+     
+     /**
+      * Obtiene el nombre del elemento.
+      *
+      * @return el nombre del elemento como un String.
+      * 
+      * Funcionalidades en las que se usa: Evaluacion operacion
+      */
+ 
+    public abstract String getNombre(); 
+     
 }
