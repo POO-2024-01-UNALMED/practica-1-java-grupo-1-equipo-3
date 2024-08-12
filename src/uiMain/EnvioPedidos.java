6776bc828acaff85621fab03c1c588e7f2ed4b54
SantiagoOchoaQuintero <159163180+SantiagoOchoaQuintero@users.noreply.github.com>
2024-08-12 12:09:03 -0500

    Update EnvioPedidos.java
    Creacion estructura base clase EnvioPedidos

---
+++ src/uiMain/EnvioPedidos.java
@@ -0,0 +1,76 @@
+/**
+ * Este módulo pertenece al paquete 'uiMain' y contiene la clase 'EnvioPedidos'.
+ * 
+ * Proporciona la funcionalidad para gestionar el envío de pedidos desde las tiendas a los clientes.
+ * 
+ * Permite seleccionar un cliente, una tienda, productos a enviar, y el transporte para realizar el envío.
+ * 
+ * AUTORES: Sebastian Estrada Villa, Valentina Luján Robledo, 
+ * Luis David Ramirez Gonzales, Santiago Ochoa Quintero
+ */
+
+
+ package uiMain;
+
+ import java.util.Scanner;
+ import java.util.ArrayList;
+ 
+ import baseDatos.Cargar;
+ import gestorAplicacion.empresa.Tienda;
+ import gestorAplicacion.externo.Cliente;
+ import gestorAplicacion.externo.TipoTransporte;
+ import gestorAplicacion.externo.Transporte;
+ import gestorAplicacion.empresa.Producto;
+ 
+ 
+ /**
+  * La clase 'EnvioPedidos' gestiona el envío de pedidos desde las tiendas a los clientes.
+  * 
+  * Proporciona un método principal que guía al usuario a través del proceso de seleccionar un cliente,
+  * una tienda, productos a enviar, y el transporte para realizar el envío.
+  */
+ 
+ public class EnvioPedidos  {
+ 
+     public static void envioPedidos() { 
+
+     boolean x = false;
+ 
+         // Manejo de opciones
+         while (x) {
+ 
+             switch (1) {
+                 
+               case 0:
+               
+               break;
+
+               case 1: 
+
+               break;
+ 
+ 
+                 case 2: 
+                         break;
+
+ 
+ 
+                 case 3: 
+                     break;
+
+ 
+ 
+                 case 4: 
+                         break;
+ 
+      
+ 
+                 case 5: 
+                             
+                             break;
+ 
+ 
+     }
+   }
+}
+}
