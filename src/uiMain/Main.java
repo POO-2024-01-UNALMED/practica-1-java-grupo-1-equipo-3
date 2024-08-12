595ea4c9bc57bf4c66a7b7a7a6e7b39f23c7770b
ValentinaLujan12 <159487000+ValentinaLujan12@users.noreply.github.com>
2024-08-11 19:08:30 -0500

    Update Main.java

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -1,5 +1,66 @@
 package uiMain;
 
 public class Main {
+    // ATRIBUTOS ----------------------------------------------------------------------------------------------------------------------------------------------------
     
+    // De clase 
+    private static final long serialVersionUID = 1L; // Versión del serializado asociada a esta clase
+    
+    // De instancia
+    // Representan las claves y valores de un diccionario
+    private Clave key;
+    private Valor value;
+    
+
+    // CONSTRUCTORES -----------------------------------------------------------------------------------------------------------------------
+    
+    /**
+     * Constructor que recibe los parámetros.
+     * 
+     * @param key Clave del par
+     * 
+     * @param value Valor del par
+     */
+
+    public Parejas(Clave key, Valor value) {
+        
+        this.key = key;
+        this.value = value;
+    }
+
+
+    /**
+     * Constructor vacío.
+     */
+
+    public Parejas(){}
+
+
+
+    // GETTERS Y SETTERS --------------------------------------------------------------------------------------------------------------------
+    
+    
+    public Clave getKey() {
+        
+        return key;
+    }
+
+    
+    public void setKey(Clave key) {
+        
+        this.key = key;
+    }
+
+    
+    public Valor getValue() {
+        
+        return value;
+    }
+
+    
+    public void setValue(Valor value) {
+        
+        this.value = value;
+    }
+}
 }
