6c265d8b31724932e014968ff6c02795c0d3dc9e
ValentinaLujan12 <159487000+ValentinaLujan12@users.noreply.github.com>
2024-08-12 17:03:53 -0500

    Update EnvioPedidos.java

--- src/uiMain/EnvioPedidos.java
+++ src/uiMain/EnvioPedidos.java
@@ -51,51 +51,51 @@ public class EnvioPedidos  {
    int numEnvioGratis=0;
    ArrayList<Producto> listaProductosPedidos = new ArrayList<Producto>();
  
-         // Manejo de opciones
-         while (interruptor) {
+        // Manejo de opciones
+        while (interruptor) {
  
             switch (eleccion) {
                  
-               case 0:
-               interruptor = false;
-               System.out.println("Has vuelto al menú anterior");
-               break;
+                case 0:
+                    interruptor = false;
+                    System.out.println("Has vuelto al menú anterior");
+                    break;
 
 
-               case 1: // Seleccionar cliente
+                case 1: // Seleccionar cliente
                     
-               System.out.print("\nSeleccione el cliente al que desea enviar: \n");
-               System.out.println("0. Volver al menu anterior \n");
-               System.out.println(Cliente.mostrarClientes());
+                    System.out.print("\nSeleccione el cliente al que desea enviar: \n");
+                    System.out.println("0. Volver al menu anterior \n");
+                    System.out.println(Cliente.mostrarClientes());
 
-               // Entero seleccionado
-               System.out.print("> ");
-               int numClienteSeleccionado = sc.nextInt(); // Pregunta al usuario
-               
-               if (numClienteSeleccionado == 0) { // Volver al menú anterior
-                   
-                   eleccion = 0;
-                   break;
+                    // Entero seleccionado
+                    System.out.print("> ");
+                    int numClienteSeleccionado = sc.nextInt(); // Pregunta al usuario
+                    
+                    if (numClienteSeleccionado == 0) { // Volver al menú anterior
+                        
+                        eleccion = 0;
+                        break;
 
-               }
+                    }
 
-               if (numClienteSeleccionado > Cliente.getListaClientes().size()) { // Número de cliente mayor a la cantidad de clientes registrados
+                    if (numClienteSeleccionado > Cliente.getListaClientes().size()) { // Número de cliente mayor a la cantidad de clientes registrados
 
-                  System.out.println("Número de cliente inválido, por favor seleccione un cliente en la lista");
-                  eleccion = 1;
-                  break;
+                        System.out.println("Número de cliente inválido, por favor seleccione un cliente en la lista");
+                        eleccion = 1;
+                        break;
 
-               } else {
+                    } else {
 
-                  clienteSeleccionado = Cliente.getListaClientes().get(numClienteSeleccionado - 1);
-                  System.out.print("Has seleccionado al cliente #" + numClienteSeleccionado + "\nEl cliente es: "
-                           + clienteSeleccionado.getNombre());
-                  eleccion = 2;
+                        clienteSeleccionado = Cliente.getListaClientes().get(numClienteSeleccionado - 1);
+                        System.out.print("Has seleccionado al cliente #" + numClienteSeleccionado + "\nEl cliente es: "
+                                    + clienteSeleccionado.getNombre());
+                        eleccion = 2;
 
-               }
+                    }
 
 
-               break;
+                     break;
  
  
                case 2: 
