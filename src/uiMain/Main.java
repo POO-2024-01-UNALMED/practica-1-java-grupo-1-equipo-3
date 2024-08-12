c3c837ab488707e8933b6eb7571a719ed6ba6361
ValentinaLujan12 <159487000+ValentinaLujan12@users.noreply.github.com>
2024-08-11 19:09:49 -0500

    Update Main.java

--- src/uiMain/Main.java
+++ src/uiMain/Main.java
@@ -1,66 +1,108 @@
-package uiMain;
-
-public class Main {
-    // ATRIBUTOS ----------------------------------------------------------------------------------------------------------------------------------------------------
-    
-    // De clase 
-    private static final long serialVersionUID = 1L; // Versión del serializado asociada a esta clase
-    
-    // De instancia
-    // Representan las claves y valores de un diccionario
-    private Clave key;
-    private Valor value;
-    
-
-    // CONSTRUCTORES -----------------------------------------------------------------------------------------------------------------------
-    
-    /**
-     * Constructor que recibe los parámetros.
-     * 
-     * @param key Clave del par
-     * 
-     * @param value Valor del par
-     */
-
-    public Parejas(Clave key, Valor value) {
-        
-        this.key = key;
-        this.value = value;
-    }
+/**
+ * Este módulo pertenece al paquete 'uiMain' y contiene la clase 'Main'.
+ * 
+ * Proporciona la funcionalidad para mostrar el menú principal de la aplicación.
+ * 
+ * Permite al usuario navegar entre las diferentes opciones del sistema.
+ * 
+ * AUTORES: Sebastian Estrada Villa, Valentina Luján Robledo, 
+ * Luis David Ramirez Gonzales, Santiago Ochoa Quintero
+ */
 
 
-    /**
-     * Constructor vacío.
-     */
+ package uiMain;
 
-    public Parejas(){}
-
-
-
-    // GETTERS Y SETTERS --------------------------------------------------------------------------------------------------------------------
-    
-    
-    public Clave getKey() {
-        
-        return key;
-    }
-
-    
-    public void setKey(Clave key) {
-        
-        this.key = key;
-    }
-
-    
-    public Valor getValue() {
-        
-        return value;
-    }
-
-    
-    public void setValue(Valor value) {
-        
-        this.value = value;
-    }
-}
-}
+ import java.util.Scanner;
+ 
+ import baseDatos.Cargar;
+ 
+ 
+ /**
+  * La clase 'Main' gestiona el menú principal de la aplicación.
+  * 
+  * Proporciona métodos para mostrar el menú y manejar las selecciones del usuario.
+  */
+ 
+ public class Main {
+     
+     static Scanner sc = new Scanner(System.in);
+     
+     
+     /**
+      * Método principal para gestionar el menú de la aplicación.
+      * 
+      * Permite al usuario navegar entre las diferentes opciones del sistema.
+      * 
+      * @param args Argumentos de la línea de comandos
+      */
+ 
+     public static void main(String[] args) {
+ 
+         Cargar.cargar();
+ 
+         int opcion;
+         
+         do{
+             System.out.println("\nMenú principal Delicia Fresca\n");
+             System.out.println("Ingrese el número de la opción que desea utilizar");
+             System.out.println("1. Envio pedidos");
+             System.out.println("2. Pago de nomina");
+             System.out.println("3. Proveer tiendas");
+             System.out.println("4. Devoluciones de productos");
+             System.out.println("5. Evaluación operación");
+             System.out.println("6. Salir\n");
+             System.out.print("> ");
+             opcion = sc.nextInt();
+ 
+ 
+             switch(opcion){
+ 
+                 case 1:
+ 
+                     EnvioPedidos.envioPedidos();
+                     break;
+ 
+ 
+                 case 2:
+ 
+                     PagoDeNomina.pagoDeNomina();
+                     break;
+ 
+ 
+                 case 3:
+ 
+                     ProveerTiendas.proveerTiendas();
+                     break;
+ 
+ 
+                 case 4:
+ 
+                     DevolucionDeProductos.devolucionDeProductos();
+                     break;
+ 
+ 
+                 case 5:
+                     EvaluacionOperacion.evaluacionOperacion();
+                     break;
+ 
+ 
+                 case 6:
+ 
+                     Cargar.guardar();
+ 
+                     System.out.println("Gracias por visitarnos");
+                     System.out.println("Vuelva pronto");
+ 
+                     break;
+                    
+                     
+                 default: // Si ingresa un número diferente al de las opciones mencionadas
+                     System.out.println("\nDigitó una opción incorrecta");
+                     
+             }
+ 
+ 
+         } while(opcion != 6);
+     
+     }
+ }
