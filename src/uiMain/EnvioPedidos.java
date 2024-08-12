00fc6ac428e23c40e81f587fdc4dadbb3b3e978c
SantiagoOchoaQuintero <159163180+SantiagoOchoaQuintero@users.noreply.github.com>
2024-08-12 11:32:37 -0500

    Correciones
    Correcion clase Envio Pedidos y creacion clase proveer tiendas

--- src/uiMain/EnvioPedidos.java
+++ src/uiMain/EnvioPedidos.java
@@ -5,10 +5,10 @@
  * 
  * Permite seleccionar tiendas, productos, y tipos de transporte para realizar el suministro.
  * 
- * AUTORES: - Sebastian Estrada Villa
- *          - Valentina Luján Robledo
- *          - Santiago Ochoa Quintero
+ * AUTORES: Sebastian Estrada Villa, Valentina Luján Robledo, 
+ * Luis David Ramirez Gonzales, Santiago Ochoa Quintero
  */
+
 
 package uiMain;
 
@@ -23,7 +23,7 @@ import gestorAplicacion.externo.TipoTransporte;
 import gestorAplicacion.externo.Transporte;
  
  
-/**
+ /**
   * La clase 'ProveerTiendas' gestiona el suministro de productos a las tiendas.
   * 
   * Proporciona métodos para seleccionar tiendas, productos, y tipos de transporte, y realizar el suministro.
@@ -32,55 +32,49 @@ import gestorAplicacion.externo.Transporte;
 public class ProveerTiendas {
     
      
-    public static void proveerTiendas() {
+      public static void proveerTiendas() {
  
-        // Atributos a usar
-        int x = 1; // Para manejar el menu
-        int eleccion = 1;
-        int escanerInt = 1;
-        int y = 1; // Para manejar el menu
-        int eleccion2 = 1;
-        int escanerInt2 = 1;
-        int z = 1; // Para manejar el menu
-        int eleccion3 = 1;
-        int escanerInt3 = 1;
+         // Atributos a usar
+         int x = 1; // Para manejar el menu
+         int eleccion = 1;
+         int escanerInt = 1;      
  
-        Scanner escaner1 = new Scanner(System.in);
-        Scanner escaner2 = new Scanner(System.in);
+         Scanner escaner1 = new Scanner(System.in);
+         Scanner escaner2 = new Scanner(System.in);
  
-        Boolean interruptor = true;
-        Tienda tiendaSeleccionada = null;
-        Producto productoSeleccionado = null;
-        Transporte transporteSeleccionado=null;
-        ArrayList<Producto> listaDeProductos;
+         Boolean interruptor = true;
+         Tienda tiendaSeleccionada = null;
+         Producto productoSeleccionado = null;
+         Transporte transporteSeleccionado=null;
+         ArrayList<Producto> listaDeProductos;
  
-        // Manejo de opciones
-        while (interruptor) {
+         // Manejo de opciones
+         while (interruptor) {
  
-           switch (eleccion) {
+            switch (eleccion) {
                  
-                case 0: {
+                  case 0: {
  
-                    interruptor = false;
-                    break;
-                }
- 
- 
-                case 1:
- 
-                    // Visto en pantalla
-                    System.out.println("\n");
-                    System.out.println("Proveer tiendas - Apartado de tiendas");
-                    System.out.println("\n0. Volver al menu anterior\n");
-                    System.out.print(Cargar.fabrica.mostrarTiendas());
-                    // Seleccionar tienda
-                    System.out.print("Seleccione la tienda a la que desea enviar: ");
-                    // Entero seleccionado
-                    x = escaner1.nextInt();
+                     interruptor = false;
+                     break;
+                 }
  
  
-                    while (interruptor) {
-                        // Se establece el intervalo en el que estan las tiendas
+                 case 1:
+ 
+                     // Visto en pantalla
+                     System.out.println("\n");
+                     System.out.println("Proveer tiendas - Apartado de tiendas");
+                     System.out.println("\n0. Volver al menu anterior\n");
+                     System.out.print(Cargar.fabrica.mostrarTiendas());
+                     // Seleccionar tienda
+                     System.out.print("Seleccione la tienda a la que desea enviar: ");
+                     // Entero seleccionado
+                     x = escaner1.nextInt();
+ 
+ 
+                     while (interruptor) {
+                         // Se establece el intervalo en el que estan las tiendas
                         if (x == 0) {
  
                            eleccion = 0;
@@ -99,19 +93,19 @@ public class ProveerTiendas {
                            x = escaner1.nextInt();
  
                         }
-                    }
+                     }
  
-                    break;
+                     break;
 
-                case 2:
-                    System.out.println("\nProveer tiendas - Apartado de productos");
-                    System.out.print("\nLa capacidad de productos por categoria para esta tienda es la siguiente: \n");
-                    System.out.println(tiendaSeleccionada.productosPorCategoria());
-                    System.out.println("\n0. Regresar al menu anterior");
-                    System.out.println(Cargar.fabrica.mostrarProductos());
-                    System.out.print("Seleccione el producto que desea enviar: ");
+                  case 2:
+                     System.out.println("\nProveer tiendas - Apartado de productos");
+                     System.out.print("\nLa capacidad de productos por categoria para esta tienda es la siguiente: \n");
+                     System.out.println(tiendaSeleccionada.productosPorCategoria());
+                     System.out.println("\n0. Regresar al menu anterior");
+                     System.out.println(Cargar.fabrica.mostrarProductos());
+                     System.out.print("Seleccione el producto que desea enviar: ");
 
-                    while (x != 0) {
+                     while (x != 0) {
                         escanerInt = escaner2.nextInt();
                         // Se establece el intervalo en el que estan los productos
 
@@ -131,35 +125,35 @@ public class ProveerTiendas {
 
                            System.out.print("Por favor seleccione un producto dentro del rango: ");
                         }
-                    }
-                    break;
+                     }
+                     break;
 
-                case 3:
+                  case 3:
 
-                    System.out.print("\nEscriba la cantidad de productos que desea proveer: ");
-                    int productoEnTiendaPorCategoria = 0;
-                    int productosMaximosEnTiendaPorCategoria = 0;
-                    String categoriaProducto = productoSeleccionado.getCategoria(); // Asumiendo que getCategoria() devuelve un String
+                     System.out.print("\nEscriba la cantidad de productos que desea proveer: ");
+                     int productoEnTiendaPorCategoria = 0;
+                     int productosMaximosEnTiendaPorCategoria = 0;
+                     String categoriaProducto = productoSeleccionado.getCategoria(); // Asumiendo que getCategoria() devuelve un String
 
-                    // Buscar en productosPorCategoria
-                    for (Parejas<String, Integer> par : tiendaSeleccionada.getProductosPorCategoria()) {
+                     // Buscar en productosPorCategoria
+                     for (Parejas<String, Integer> par : tiendaSeleccionada.getProductosPorCategoria()) {
                         
                         if (par.getKey().equals(categoriaProducto)) {
                            productoEnTiendaPorCategoria = par.getValue();
                            break;
                         }
-                    }
+                     }
 
-                    // Buscar en cantidadPorCategoria
-                    for (Parejas<String, Integer> par : tiendaSeleccionada.getCantidadPorCategoria()) {
+                     // Buscar en cantidadPorCategoria
+                     for (Parejas<String, Integer> par : tiendaSeleccionada.getCantidadPorCategoria()) {
                         
                         if (par.getKey().equals(categoriaProducto)) {
                            productosMaximosEnTiendaPorCategoria = par.getValue();
                            break;
                         }
-                    }
+                     }
                      
-                    while (true) {
+                     while (true) {
                         
                         escanerInt = escaner2.nextInt();
                         
@@ -179,82 +173,82 @@ public class ProveerTiendas {
                            
                            System.out.print("Por favor seleccione una cantidad en el limite de la tienda por categoria: ");
                         }
-                    }
+                     }
 
-                    break;
+                     break;
                      
-                case 4:
+                  case 4:
 
-                    // seleccionar tipo de transporte
+                     // seleccionar tipo de transporte
                      
-                    int PesoTotalProductos = escanerInt * ((int) Math.round(productoSeleccionado.getPeso()));
-                    System.out.println("\n\nSeleccione en que medio de transporte quiere enviar este producto");
-                    System.out.println(
-                            "\nLos tipos de transporte han sido filtrados de manera que solo puede seleccionar los que puedan soportar el peso de su producto.");
+                     int PesoTotalProductos = escanerInt * ((int) Math.round(productoSeleccionado.getPeso()));
+                     System.out.println("\n\nSeleccione en que medio de transporte quiere enviar este producto");
+                     System.out.println(
+                             "\nAdvertencia: Los tipos de transporte han sido filtrados de manera que solo puede seleccionar los que puedan soportar el peso de su producto.");
  
-                    System.out.println("0. Regresar al menu anterior");
+                     System.out.println("0. Regresar al menu anterior");
  
-                    // TipoTransporte tipoTransportes;
-                    ArrayList<TipoTransporte> listaTransFiltrada = new ArrayList<TipoTransporte>();
-                    listaTransFiltrada = TipoTransporte.transporteSegunCarga(PesoTotalProductos);
-                    // System.out.printlnLoad.tipoTransportes.mostrarTipoTransporteSegunCarga(productoSeleccionado));
-                    System.out.println(TipoTransporte.mostrarTransporteSegunCarga(listaTransFiltrada));
-                    System.out.println("Seleccione el número del tipo de transporte: ");
-                    System.out.print("> ");
+                     // TipoTransporte tipoTransportes;
+                     ArrayList<TipoTransporte> listaTransFiltrada = new ArrayList<TipoTransporte>();
+                     listaTransFiltrada = TipoTransporte.transporteSegunCarga(PesoTotalProductos);
+                     // System.out.printlnLoad.tipoTransportes.mostrarTipoTransporteSegunCarga(productoSeleccionado));
+                     System.out.println(TipoTransporte.mostrarTransporteSegunCarga(listaTransFiltrada));
+                     System.out.println("Seleccione el número del tipo de transporte: ");
+                     System.out.print("> ");
  
-                    while(true){
+                     while(true){
  
-                    int numTransporteSeleccionado = escaner2.nextInt();
+                     int numTransporteSeleccionado = escaner2.nextInt();
                      
-                    if (numTransporteSeleccionado == 0) {
+                     if (numTransporteSeleccionado == 0) {
                          
                         eleccion = 2;
                         break;
-                    }
+                     }
  
-                    if (numTransporteSeleccionado > listaTransFiltrada.size() || numTransporteSeleccionado < 0) {
+                     if (numTransporteSeleccionado > listaTransFiltrada.size() || numTransporteSeleccionado < 0) {
                          
                         System.out.print("Número de transporte inválido, por favor seleccione un producto en la lista \n> ");
-                    }
+                     }
 
-                    else {
+                     else {
                          
                         transporteSeleccionado = TipoTransporte.seleccionarTransporte(listaTransFiltrada,
-                                numTransporteSeleccionado);
+                                 numTransporteSeleccionado);
                         System.out.print("Ha seleccionado el transporte #" + (numTransporteSeleccionado)
-                                + "\nLa tienda se proveerá por: " + transporteSeleccionado.getTipo().getNombre());
+                                 + "\nLa tienda se proveerá por: " + transporteSeleccionado.getTipo().getNombre());
                         eleccion = 5;
                         break;
-                    }
+                     }
                   
-                }
+                  }
  
-                    break;
+                     break;
                   
-                 case 5:
+                  case 5:
 
-                    //Paso final de la funcionalidad, llevamos a cabo el envio de los productos.
+                     //Paso final de la funcionalidad, llevamos a cabo el envio de los productos.
 
-                    listaDeProductos = Cargar.fabrica.cantidadProductos(escanerInt, productoSeleccionado); // meter los productos en el camion
-                    transporteSeleccionado.suministrarProducto(tiendaSeleccionada, listaDeProductos); // Se descargan los productos en la tienda,  luego de comprobar que sea la tienda correcta
+                     listaDeProductos = Cargar.fabrica.cantidadProductos(escanerInt, productoSeleccionado); // meter los productos en el camion
+                     transporteSeleccionado.suministrarProducto(tiendaSeleccionada, listaDeProductos); // Se descargan los productos en la tienda,  luego de comprobar que sea la tienda correcta
  
-                    if (transporteSeleccionado.getTienda().equals(tiendaSeleccionada) == true) {
+                     if (transporteSeleccionado.getTienda().equals(tiendaSeleccionada) == true) {
                          
                         tiendaSeleccionada.descargarProducto(transporteSeleccionado);
                         System.out
-                                .println("\nEl producto fue enviado con exito ahora la tienda tiene \nPRODUCTOS: "
-                                        + tiendaSeleccionada.cantidadProductos());
+                                 .println("\nEl producto fue enviado con exito ahora la tienda tiene \nPRODUCTOS: "
+                                         + tiendaSeleccionada.cantidadProductos());
  
-                    } else {
+                     } else {
  
                         System.out.println("El envio no se pudo realizar a esa tienda");
-                    }
+                     }
  
-                    // Ciclo final para ver si sale o se reinicia la funcionalidad
-                    System.out.println("\n0.Volver al menu principal" + "\n" + "1. Proveer más tiendas");
-                    System.out.print("> ");
+                     // Ciclo final para ver si sale o se reinicia la funcionalidad
+                     System.out.println("\n0.Volver al menu principal" + "\n" + "1. Proveer más tiendas");
+                     System.out.print("> ");
  
-                    while (true) {
+                     while (true) {
  
                         escanerInt = escaner2.nextInt();
                          
@@ -272,13 +266,13 @@ public class ProveerTiendas {
                              
                            System.out.println("Seleccione una de las opciones disponibles: ");
                         }
-                    }
-                    break;
+                     }
+                     break;
                      
                      
             }
  
-        }
+         }
  
    }
-}
+ }
