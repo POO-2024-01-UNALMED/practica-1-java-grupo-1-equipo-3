8496281c2a4c77a3d6fd0f55014e3353bc8d8f7e
ValentinaLujan12 <159487000+ValentinaLujan12@users.noreply.github.com>
2024-08-12 17:18:47 -0500

    final envio pedidos

--- src/uiMain/EnvioPedidos.java
+++ src/uiMain/EnvioPedidos.java
@@ -266,72 +266,72 @@ public class EnvioPedidos  {
  
                 case 5: // retorna factura
                      
-                     System.out.println("\nDigite el día del mes entre 1 y 30: ");
-                     System.out.print("> ");
-                     int dia = 1;
+                    System.out.println("\nDigite el día del mes entre 1 y 30: ");
+                    System.out.print("> ");
+                    int dia = 1;
                      
-                     while(true){
+                    while(true){
  
-                         dia = sc.nextInt();
+                        dia = sc.nextInt();
                          
-                         if (dia >= 1 && dia <=30){
+                        if (dia >= 1 && dia <=30){
                              
-                             break;
+                            break;
  
-                         }else{
+                        }else{
  
-                             System.out.println("Seleccione un numero dentro del rango");
-                             System.out.print("> ");
+                            System.out.println("Seleccione un numero dentro del rango");
+                            System.out.print("> ");
  
-                         }
-                     }
+                        }
+                    }
  
-                     System.out.println("\n-------------------------------------\n"+ "Factura generada en la tienda " 
-                     +tiendaSeleccionada.getNombre() + "\nA nombre del cliente: " + clienteSeleccionado.getNombre()+"\n" 
-                     +tiendaSeleccionada.enviarPedido(new ArrayList<>(listaProductosPedidos),
-                             transporteSeleccionado, clienteSeleccionado, dia, Cargar.fabrica.getOperario())+"\n-------------------------------------\n\n\n");
+                    System.out.println("\n-------------------------------------\n"+ "Factura generada en la tienda " 
+                    +tiendaSeleccionada.getNombre() + "\nA nombre del cliente: " + clienteSeleccionado.getNombre()+"\n" 
+                    +tiendaSeleccionada.enviarPedido(new ArrayList<>(listaProductosPedidos),
+                            transporteSeleccionado, clienteSeleccionado, dia, Cargar.fabrica.getOperario())+"\n-------------------------------------\n\n\n");
                      
-                     if(numEnvioGratis==1){
+                    if(numEnvioGratis==1){
  
-                         transporteSeleccionado.reestablecerPrecioTransporte();
+                        transporteSeleccionado.reestablecerPrecioTransporte();
                      
-                     }
+                    }
                     
-                         listaProductosPedidos.clear();
+                        listaProductosPedidos.clear();
  
-                     // Realiza una copia de la listaProductosPedidos
-                     PesoTotalProductos = 0;
-                     System.out.println("¿Desea hacer otro envio o volver al menu principal? ");
-                     System.out.println("0. Volver al menu principal");
-                     System.out.println("1. Realizar otro  envio");
-                     System.out.print("> ");
-                     int opcion = sc2.nextInt();
+                    // Realiza una copia de la listaProductosPedidos
+                    PesoTotalProductos = 0;
+                    System.out.println("¿Desea hacer otro envio o volver al menu principal? ");
+                    System.out.println("0. Volver al menu principal");
+                    System.out.println("1. Realizar otro  envio");
+                    System.out.print("> ");
+                    int opcion = sc2.nextInt();
  
-                     while (true) {
+                    while (true) {
  
-                         if (opcion == 0) {
+                        if (opcion == 0) {
                              
-                             eleccion = 0;
-                             interruptor = false;
-                             break;
+                            eleccion = 0;
+                            interruptor = false;
+                            break;
  
-                         } else if (opcion == 1) {
+                        } else if (opcion == 1) {
                              
-                             eleccion = 1;
-                             break;
+                            eleccion = 1;
+                            break;
  
-                         } else {
+                        } else {
                              
-                             System.out.println(" La opcion que digitó es incorrecta ");
+                            System.out.println(" La opcion que digitó es incorrecta ");
  
-                         }
-                     }
+                        }
+                    }
                      
-                     break;
+                    break;
  
-             }
+            }
  
-         }
+        }
  
-     }
- }
+    }
+}
